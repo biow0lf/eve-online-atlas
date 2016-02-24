90cffaf088aaa3e58261d8c0b7a3d2b6ac9014d3
Zachary Lovin <zlovin@Zacharys-MacBook-Pro-2.local>
2016-02-23 21:46:30 -0600

    Add default route for /api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  def version_info
+    [200,
+     { 'Content-Type' => 'application/json' },
+     [{ app_version: APP_VERSION,
+        ruby_version: "#{RUBY_ENGINE}-#{RUBY_VERSION}-p#{RUBY_PATCHLEVEL}",
+        rails_version: Rails.version
+      }.to_json]]
+  end
+
   match '*any' => 'application#options', :via => [:options]
 
   root 'crest#index'
@@ -7,6 +17,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      root to: proc { version_info }
       resources :items, only: [:index, :show]
       resources :solarsystems, only: [:index, :show]
     end
