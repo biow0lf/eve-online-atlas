bbc63401a1dd81349cc3d03c5db67059b9837ae7
Zachary Lovin <zlovin@Zacharys-MacBook-Pro-2.local>
2016-02-25 09:17:59 -0600

    Move token refresh to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+require 'logger'
+require 'base64'
 class UsersController < ApplicationController
   before_action :find_user
 
@@ -17,5 +19,21 @@ class UsersController < ApplicationController
     @user = User.find(session[:user_id])
   rescue ActiveRecord::RecordNotFound
     render json: { error: 'Session user_id is invalid' }, status: :bad_request
+  end
+
+  def refresh_token_if_expired
+    if @user.token_expired?
+      include HTTParty
+      headers = { Authorization: 'Basic ' + Base64.encode64("#{ENV['CREST_CLIENT_ID']}:#{ENV['CREST_CLIENT_SECRET']}") }
+      body = { grant_type: 'refresh_token', refresh_token: self.refreshToken}
+      response = HTTParty.post('https://login.eveonline.com/oauth/token', body: body.to_json, headers: headers )
+
+      log = Logger.new('log/blah.log')
+
+      log.debug response.body
+      log.debug response.body.inspect
+
+      @user.update(token: response.body['token'], expiry: response.body['expires_at'].to_i.seconds + DateTime.now)
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
-require 'base64'
-require 'logger'
-require 'HTTParty'
 class User < ActiveRecord::Base
-  include HTTParty
   def self.create_from_omniauth(auth)
     create! do |user|
       user.uid = auth['uid']
@@ -11,23 +7,6 @@ class User < ActiveRecord::Base
       user.name = auth['info']['name']
       user.token = auth['credentials']['token']
       user.expiry = auth['info']['expires_on']
-    end
-  end
-
-  def refresh_token_if_expired
-    if token_expired?
-
-      headers = { Authorization: 'Basic ' + Base64.encode64("#{ENV['CREST_CLIENT_ID']}:#{ENV['CREST_CLIENT_SECRET']}") }
-      body = { grant_type: 'refresh_token', refresh_token: self.refreshToken}
-      response = HTTParty.post('https://login.eveonline.com/oauth/token', body: body.to_json, headers: headers )
-
-      log = Logger.new('log/blah.log')
-
-      log.debug response.body
-      log.debug response.body.inspect
-
-      self.token = response.body['token']
-      self.expiry = response.body['expires_at'].to_i.seconds + DateTime.now
     end
   end
 
