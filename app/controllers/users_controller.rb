bc8f9d23368c983de9e15bff52aa5050466fbd40
Zachary Lovin <zlovin@Zacharys-MacBook-Pro-2.local>
2016-02-25 08:42:49 -0600

    Refresh user token on index if expired

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,21 @@
 class UsersController < ApplicationController
-  # before_action :authenticate_user!
-  # before_action :current_user?, except: [:index]
+  before_action :find_user
 
   def index
-    # @users = User.all
-    user = User.find(session[:user_id]) if session[:user_id]
-    render json: user.as_json
+    return render status: :bad_request unless @user
+    @user.refresh_token_if_expired
+    render json: @user.as_json
   end
 
   def show
     # @user = User.find(params[:id])
+  end
+
+  private
+
+  def find_user
+    @user = User.find(session[:user_id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Session user_id is invalid' }, status: :bad_request
   end
 end
