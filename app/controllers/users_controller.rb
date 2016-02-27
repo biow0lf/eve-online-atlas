cef85caf4f5d1d165778706eee48e2bc00b73e22
Zachary Lovin <zlovin@Zacharys-MacBook-Pro-2.local>
2016-02-27 12:16:46 -0600

    Don't make /user return :bad_request if not signed in Add aria-label to SSO button to silence console yammering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :find_user
 
   def index
-    return render status: :bad_request unless @user
+    return render json: {} unless @user
     refresh_token_if_expired
     render json: @user.as_json
   end
@@ -16,9 +16,7 @@ class UsersController < ApplicationController
   private
 
   def find_user
-    @user = User.find(session[:user_id])
-  rescue ActiveRecord::RecordNotFound
-    render json: { error: 'Session user_id is invalid' }, status: :bad_request
+    @user = User.find(session[:user_id]) if session[:user_id]
   end
 
   def refresh_token_if_expired
