8b1fec8fd6dbb366ea2dceb3dc39aa77f0be08e5
Zachary Lovin <zlovin@Zacharys-MacBook-Pro-2.local>
2016-03-01 23:15:47 -0600

    Change logger stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,3 @@
-require 'logger'
 require 'base64'
 class UsersController < ApplicationController
   before_action :find_user
@@ -36,16 +35,13 @@ class UsersController < ApplicationController
 
   def refresh_token_if_expired
     return unless @user.token_expired?
-    log = Logger.new('log/location.log')
-    log.debug @user
-
+    puts @user
     headers = { Authorization: 'Basic ' + Base64.encode64("#{ENV['CREST_CLIENT_ID']}:#{ENV['CREST_CLIENT_SECRET']}") }
     body = { grant_type: 'refresh_token', refresh_token: refreshToken }
     response = HTTParty.post('https://login.eveonline.com/oauth/token', body: body.to_json, headers: headers)
 
-    log = Logger.new('log/blah.log')
-    log.debug response.body
-    log.debug response.body.inspect
+    puts response.body
+    puts response.body.inspect
 
     @user.update(token: response.body['token'], expiry: response.body['expires_at'].to_i.seconds + DateTime.now)
   end
