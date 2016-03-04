c448617d50c055d3de349b1080f81cfde85f2a36
George Dietrich <yomoejoe@gmail.com>
2016-03-04 21:59:42 +0100

    Add models for agent divisions and agent type Add new models to agent model

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -4,4 +4,6 @@ class Agent < ActiveRecord::Base
 
   belongs_to :station, foreign_key: 'locationID', primary_key: 'locationID'
   has_one :agentName, foreign_key: 'itemID'
+  has_one :agentType, foreign_key: 'agentTypeID', primary_key: 'agentTypeID'
+  has_one :agentDivision, foreign_key: 'divisionID', primary_key: 'divisionID'
 end
